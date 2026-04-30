ac58a385c9d89fdfe56e4a17750fefd27068cee7
Alex Yao <alexyao@Alexs-MacBook-Pro.local>
2026-05-01 10:10:32 +1200

    Database up and running

--- app.py
+++ app.py
@@ -3,10 +3,10 @@
 # imports
 from flask import Flask, render_template, request, flash, session, redirect
 from flask_session import Session
-from flask_login import LoginManager, UserMixin
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
-import hashlib
 
 
 # Constants
@@ -24,18 +24,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///lost_and_found.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-# tables
-class User(db.Model):
+# FlaskSQAlchemy tables
+class User(db.Model, UserMixin):
+    '''Database table containing the User information'''
     __tablename__ = 'user'
     user_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(128), nullable=False) 
-    school_code = db.Column(db.String(20), nullable=False)
+    school_code = db.Column(db.String(20), nullable=False, unique=True)
     clearance = db.Column(db.Integer, default=4)
 
 
 class LostItem(db.Model):
+    '''Database table containing the lost item information'''
     __tablename__ = 'lost_item'
     item_id = db.Column(db.Integer, primary_key=True)
     finder_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
@@ -49,17 +51,18 @@ class LostItem(db.Model):
 
 
 class Colour(db.Model):
+    '''Database table containing a list of colours'''
     __tablename__ = 'colour'
     colour_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
 
 
+# table in the middle that links LostItem and Colour
 lostitem_colour = db.Table(
     'lostitem_colour', 
     db.Column('iid', db.Integer, db.ForeignKey('lost_item.item_id')),
     db.Column('cid', db.Integer, db.ForeignKey('colour.colour_id'))
 )
-
 
 
 @app.route('/test')
@@ -82,14 +85,13 @@ def create():
     return 'donezo'
 
 
-
 # Login Stuff
-# login_manager = LoginManager()
-# login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-# @login_manager.user_loader
-# def load_user(user_id):
-#     return User.get(user_id)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 # routes
@@ -158,10 +160,41 @@ def login():
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     '''
-    Docstring for signup
+    This is the route that leads to the signup page. This page allows the
+    user to sign up using a username and password
     '''
+
+    if request.method == 'POST':
+        print('form done')
+        first_name = request.form.get('username').rstrip()
+        last_name = request.form.get('username').rstrip()
+        school_code = request.form.get('school_code').rstrip()
+        password = request.form.get('password').rstrip()
+        print('password')
+        # Checking there is no blank inputs
+        if first_name is None or last_name is None or password is None:
+            flash('Provide valid input')
+            return render_template("signup.html",
+                                   title="Sign Up")
+        
+        # Succesful account creation
+        print('success')
+        add = User(first_name=first_name, last_name=last_name, password=password, school_code=school_code)
+        db.session.add(add)
+        db.session.commit()
+        
+    print('route')    
     return render_template('signup.html',
                            title='Signup',)
+
+
+# error handlers
+@app.errorhandler(404)
+def page_not_found(e):
+    '''
+    Custom 404 page not found page
+    '''
+    return render_template("404.html", title="Page Not Found"), 404
 
 
 if __name__ == "__main__":
